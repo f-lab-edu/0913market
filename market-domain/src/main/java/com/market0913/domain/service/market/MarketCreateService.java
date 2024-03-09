6ba02f934d47c2b4032c0bbd4b61260013536b08
spring0913 <bbomi0913@gmail.com>
2024-03-09 13:24:01 +0900

    refactor(#15): Product Controller, Service 삭제

--- market-domain/src/main/java/com/market0913/domain/service/market/MarketCreateService.java
+++ market-domain/src/main/java/com/market0913/domain/service/market/MarketCreateService.java
@@ -1,15 +1,23 @@
 package com.market0913.domain.service.market;
 
+import com.market0913.domain.model.category.Category;
 import com.market0913.domain.model.market.Market;
 import com.market0913.domain.model.market.MarketCreator;
 import com.market0913.domain.model.market.MarketDto;
+import com.market0913.domain.model.member.Member;
+import com.market0913.domain.model.member.MemberType;
+import com.market0913.domain.model.product.Product;
 import com.market0913.domain.model.product.ProductCreator;
 import com.market0913.domain.model.product.ProductDto;
+import com.market0913.domain.repository.CategoryRepository;
 import com.market0913.domain.repository.MarketRepository;
-import com.market0913.domain.service.product.ProductCreateService;
+import com.market0913.domain.repository.MemberRepository;
+import com.market0913.domain.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
 @Service
@@ -18,12 +26,24 @@ public class MarketCreateService {
 
     private final MarketRepository marketRepository;
 
-    private final ProductCreateService productCreateService;
+    private final ProductRepository productRepository;
+    private final CategoryRepository categoryRepository;
+    private final MemberRepository memberRepository;
 
     public MarketDto createMarket(MarketCreator marketCreator) {
         ProductCreator productCreator = new ProductCreator(marketCreator);
-        ProductDto productDto = productCreateService.createProduct(productCreator);
+        ProductDto productDto = createProduct(productCreator);
         Market market = MarketCreator.createMarket(marketCreator, productDto);
         return MarketDto.from(marketRepository.save(market));
+    }
+
+    private ProductDto createProduct(ProductCreator productCreator) {
+        Member seller = memberRepository.findByMemberIdAndType(productCreator.getSellerId(), MemberType.SELLER)
+                .orElseThrow(() -> new NoSuchElementException("판매자 정보를 찾을 수 없습니다."));
+
+        Category category = categoryRepository.findByName(productCreator.getCategory());
+        Product product = ProductCreator.createProduct(seller, category, productCreator);
+
+        return ProductDto.from(productRepository.save(product));
     }
 }
